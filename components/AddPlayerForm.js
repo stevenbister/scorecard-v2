62f2ecd09935bd68f3cc994eb7ffbeafe6882f2f
Steven Bister <stevebister@gmail.com>
2022-02-01 18:23:00 +0000

    Refactor: Lift state and functions up out of the components and into the parent
    This will let us update our players list when the form is submitted

--- components/AddPlayerForm.js
+++ components/AddPlayerForm.js
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import { supabase } from '../utils/supabaseClient'
-
-// TODO: Add react context so we can pass the session state through all of our components
-// TODO: Add error handling component
-const AddPlayerForm = ({userSession}) => {
-  const [playerName, setPlayerName] = useState(null)
-
-  const handleUpdate = async (e) => {
-    e.preventDefault()
-
-    try {
-      let { data, error, status } = await supabase.from('players').insert({
-          profile_id: userSession.user.id,
-          player_name: playerName,
-        })
-
-      if (error && status !== 406) {
-        throw error
-      }
-
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
+const AddPlayerForm = ({handleSubmit, onChange}) => {
   return (
-    <form>
+    <form onSubmit={handleSubmit} >
         <label htmlFor="name">Player Name</label>
-        <input id="name" type="text" value={playerName || ''} onChange={(e) => setPlayerName(e.target.value)} />
+        <input id="name" type="text" onChange={onChange} />
 
-        <button onClick={handleUpdate}>Add</button>
+        <button>Add</button>
     </form>
   )
 }
--- pages/players.js
+++ pages/players.js
@@ -1,15 +1,75 @@
+import { useState, useEffect } from 'react'
 import AddPlayerForm from '../components/AddPlayerForm'
 import ListPlayers from '../components/ListPlayers'
+import { supabase } from '../utils/supabaseClient'
 
-const Profile = ({userSession}) => {
+// TODO: Add error handling component
+const Players = ({userSession}) => {
+  const [playerName, setPlayerName] = useState(null)
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    getPlayers()
+    // deletePlayers() // ! Remove this this is only while we mess around with the db
+  }, [players])
+
+
+  const deletePlayers = async () => {
+    try {
+      const { data, error } = await supabase.from('players').delete().match({ player_name: 'test player 7' })
+
+      if (error) throw error
+
+    } catch (error) {
+      throw error
+    }
+  }
+
+  const getPlayers = async () => {
+    try {
+      const user = supabase.auth.user()
+
+      let { data, error, status } = await supabase
+        .from('players').select('id, player_name, created_at, profile_id').eq('profile_id', user.id)
+
+      if (error && status !== 406) throw error
+
+      await setPlayers(data)
+
+    } catch (error) {
+      throw error
+    }
+  }
+
+  const createPlayer = async (e) => {
+    e.preventDefault()
+
+    try {
+      let { data, error, status } = await supabase.from('players').insert({
+          profile_id: userSession.user.id,
+          player_name: playerName,
+        })
+
+      if (error && status !== 406) throw error
+
+    } catch (error) {
+      throw error
+    }
+  }
+
   return (
     <>
       <h1>Players</h1>
 
-      <AddPlayerForm userSession={userSession} />
-      <ListPlayers />
+      <AddPlayerForm
+        userSession={userSession}
+        handleSubmit={createPlayer}
+        onChange={(e) => setPlayerName(e.target.value)}
+        playerName={playerName}
+      />
+      <ListPlayers players={players} />
     </>
   )
 }
 
-export default Profile
+export default Players
