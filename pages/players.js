9e96215f0653d26d2c89833466def35c78092320
Steven Bister <stevebister@gmail.com>
2022-02-02 17:06:07 +0000

    Refactor: Moves player functions out into a util file. Keeps our players.js component tidy

--- pages/players.js
+++ pages/players.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import AddPlayerForm from '../components/AddPlayerForm'
 import ListPlayers from '../components/ListPlayers'
-import { supabase } from '../utils/supabaseClient'
+import { getPlayers, createPlayer } from '../utils/managePlayers'
 
 // TODO: Add error handling component
 const Players = ({userSession}) => {
@@ -9,53 +9,12 @@ const Players = ({userSession}) => {
   const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    getPlayers()
-    // deletePlayers() // ! Remove this this is only while we mess around with the db
+    (async () => {
+        const data = await getPlayers()
+
+        setPlayers(data)
+    })()
   }, [players])
-
-
-  const deletePlayers = async () => {
-    try {
-      const { data, error } = await supabase.from('players').delete().match({ player_name: 'test player 7' })
-
-      if (error) throw error
-
-    } catch (error) {
-      throw error
-    }
-  }
-
-  const getPlayers = async () => {
-    try {
-      const user = supabase.auth.user()
-
-      let { data, error, status } = await supabase
-        .from('players').select('id, player_name, created_at, profile_id').eq('profile_id', user.id)
-
-      if (error && status !== 406) throw error
-
-      await setPlayers(data)
-
-    } catch (error) {
-      throw error
-    }
-  }
-
-  const createPlayer = async (e) => {
-    e.preventDefault()
-
-    try {
-      let { data, error, status } = await supabase.from('players').insert({
-          profile_id: userSession.user.id,
-          player_name: playerName,
-        })
-
-      if (error && status !== 406) throw error
-
-    } catch (error) {
-      throw error
-    }
-  }
 
   return (
     <>
@@ -63,7 +22,7 @@ const Players = ({userSession}) => {
 
       <AddPlayerForm
         userSession={userSession}
-        handleSubmit={createPlayer}
+        handleSubmit={(e) => createPlayer(e, playerName)}
         onChange={(e) => setPlayerName(e.target.value)}
         playerName={playerName}
       />
