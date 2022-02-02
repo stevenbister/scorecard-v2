0d1e7814ce4095fa83d129e76358ef8bf44267f2
Steven Bister <stevebister@gmail.com>
2022-02-02 21:50:32 +0000

    Adds function props and state to our player component
    We can now control adding/removing players to the game and save them in our localstorage

--- components/Player.js
+++ components/Player.js
@@ -1,10 +1,23 @@
+import { useState } from "react"
 import { deletePlayer } from "../utils/managePlayers"
 
-const Player = ({playerName}) => {
+const Player = ({playerName, addToGame, removeFromGame}) => {
+  const [playerIsInGame, setPlayerIsInGame] = useState(false)
+
   return(
     <div>
       <span>{playerName}</span>
-      <button onClick={() => deletePlayer(playerName)}>Remove</button>
+
+      <button
+        onClick={() => {addToGame(playerName), setPlayerIsInGame(true)}} disabled={playerIsInGame}>
+        Add to game
+      </button>
+
+      <button onClick={() => {removeFromGame(playerName), setPlayerIsInGame(false)}} disabled={!playerIsInGame}>
+        Remove from game
+      </button>
+
+      <button onClick={() => {removeFromGame(playerName), deletePlayer(playerName)}}>Delete</button>
     </div>
   )
 }
