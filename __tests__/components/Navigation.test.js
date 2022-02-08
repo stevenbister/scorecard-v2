388aad30bff69b4be60ec9b4f53ade25b155c495
Steven Bister <stevebister@gmail.com>
2022-02-08 17:18:55 +0000

    Test: Adds test to confirm that the nav component is rendering

---
+++ __tests__/components/Navigation.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import Navigation from '../../components/Navigation'
+
+describe('Navigation', () => {
+  test('renders the nav', () => {
+    render(<Navigation />)
+
+    expect(screen.getByRole('navigation')).toBeInTheDocument()
+  })
+})
