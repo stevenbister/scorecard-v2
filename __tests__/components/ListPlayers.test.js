cbfb4199837f4984fb2e0d01963ad163180ce2fb
Steven Bister <stevebister@gmail.com>
2022-02-08 17:15:34 +0000

    Test: Adds tests for our list items component

---
+++ __tests__/components/ListPlayers.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+import ListPlayers from '../../components/ListPlayers'
+
+describe('ListPlayers', () => {
+  test('renders no players when the player list is empty', () => {
+    render(<ListPlayers players={[]} />)
+
+    expect(screen.queryByRole('list')).not.toBeInTheDocument()
+    expect(screen.queryByText(/no players/i)).toBeInTheDocument()
+  })
+
+  test('renders the player controls in a list', () => {
+    // Generate some fake data to create the players list.
+    // As we're only checking to see if the list gets rendered
+    // it doesn't matter too much that we're not connecting to the
+    // api to generate it.
+    const fakePlayers = [
+      {
+        player_name: 'player 1',
+        id: 1,
+      },
+      {
+        player_name: 'player 2',
+        id: 2,
+      },
+      {
+        player_name: 'player 3',
+        id: 3,
+      },
+    ]
+    render(<ListPlayers players={fakePlayers} />)
+
+    const listOfPlayers = screen.queryAllByRole('listitem')
+
+    expect(listOfPlayers).toHaveLength(3)
+    expect(listOfPlayers[0]).toHaveTextContent('player 1')
+    expect(listOfPlayers[1]).toHaveTextContent('player 2')
+    expect(listOfPlayers[2]).toHaveTextContent('player 3')
+  })
+})
