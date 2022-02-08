c494e4a0c8458c48bfc65204364ee8dba1ee39b5
Steven Bister <stevebister@gmail.com>
2022-02-08 16:26:42 +0000

    Feat: Enable our .env settings
    Need to load our .env files within our jest setup otherwise they will return undefined causing our tests to break

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom/extend-expect'
+import { loadEnvConfig } from '@next/env'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async () => {
+  const projectDir = process.cwd()
+  loadEnvConfig(projectDir)
+}
