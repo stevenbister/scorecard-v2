bd5be8bb5baf8089b106c95286404e285a0ebf94
Steven Bister <stevebister@gmail.com>
2022-03-18 12:57:44 +0000

    Feat: Adds player component
    This will display the players username and the avatar

---
+++ components/Player.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+import { Box, Text } from '@chakra-ui/react'
+import Avatar from 'boring-avatars'
+import { useProfile } from '../lib/profile/useProfile'
+
+const Player = ({ user }) => {
+  const { username } = useProfile(user)
+
+  return (
+    <Box
+      borderBottom="1px"
+      borderBottomColor="gray.500"
+      display="flex"
+      alignItems="center"
+      p={2}
+    >
+      <Avatar
+        size={40}
+        name={username}
+        variant="beam"
+        colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
+      />
+
+      <Text fontWeight="semibold" px={4}>
+        {username}
+      </Text>
+    </Box>
+  )
+}
+
+Player.propTypes = {
+  user: PropTypes.object,
+}
+
+export default Player
