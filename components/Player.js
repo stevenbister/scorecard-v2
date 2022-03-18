cc9d7cc8353929bfb66b160fcb329e1a97df19ff
Steven Bister <stevebister@gmail.com>
2022-03-18 17:57:46 +0000

    Feat: increases font size of the player card

--- components/Player.js
+++ components/Player.js
@@ -21,7 +21,7 @@ const Player = ({ user }) => {
         colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
       />
 
-      <Text fontWeight="semibold" px={4}>
+      <Text fontWeight="semibold" fontSize={20} px={4}>
         {username}
       </Text>
     </Box>
