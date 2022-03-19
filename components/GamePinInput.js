4bbdb4b5f760b060e276199c6d287de551486ca7
Steven Bister <stevebister@gmail.com>
2022-03-19 16:24:03 +0000

    Feat: Adds styles to our pin input form

--- components/GamePinInput.js
+++ components/GamePinInput.js
@@ -1,24 +1,53 @@
 import { useContext } from 'react'
 import { GameContext } from '../lib/game/GameContext'
-import { Button, PinInput, PinInputField } from '@chakra-ui/react'
+import Link from 'next/link'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  PinInput,
+  PinInputField,
+  HStack,
+  VStack,
+} from '@chakra-ui/react'
 
 const GamePinInput = () => {
   const { joinGame, loading } = useContext(GameContext)
 
   return (
-    <form onSubmit={(e) => joinGame(e)} name="joinGame">
-      <PinInput>
-        <PinInputField />
-        <PinInputField />
-        <PinInputField />
-        <PinInputField />
-        <PinInputField />
-      </PinInput>
+    <>
+      <form onSubmit={(e) => joinGame(e)} id="joinGame" name="joinGame">
+        <VStack align="stretch" spacing={4}>
+          <FormControl as="fieldset">
+            <FormLabel as="legend" size="3xl" mx="auto">
+              Enter pin
+            </FormLabel>
 
-      <Button type="submit" isLoading={loading}>
-        Join
-      </Button>
-    </form>
+            <HStack justify="center">
+              <PinInput>
+                <PinInputField />
+                <PinInputField />
+                <PinInputField />
+                <PinInputField />
+                <PinInputField />
+              </PinInput>
+            </HStack>
+          </FormControl>
+        </VStack>
+      </form>
+
+      <VStack align="stretch" p={20} style={{ marginTop: 'auto' }}>
+        <Button form="joinGame" type="submit" isLoading={loading}>
+          Join game
+        </Button>
+
+        <Link href="/" passHref>
+          <Button as="a" variant="outline">
+            Exit
+          </Button>
+        </Link>
+      </VStack>
+    </>
   )
 }
 
