b510fc3e8dead794a2005a8c1942777bdce115e8
Steven Bister <stevebister@gmail.com>
2022-03-20 20:04:54 +0000

    Feat: Add redirect to appropriate page when user object exists

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,5 +1,17 @@
+import { useEffect } from 'react'
+import Router from 'next/router'
 import Auth from '../components/Auth'
+import { useAuth } from '../lib/auth/useAuth'
 
-const SignIn = () => <Auth heading="Sign in" />
+const SignIn = () => {
+  const { user } = useAuth()
+
+  useEffect(() => {
+    if (user) {
+      Router.push('/')
+    }
+  }, [user])
+  return <Auth heading="Sign in" />
+}
 
 export default SignIn
