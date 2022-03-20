eb04476a03464bee59428b47f16170958768055d
Steven Bister <stevebister@gmail.com>
2022-03-20 17:21:43 +0000

    Refactor: Moved content out of game component
    Now rendering it directly in the home component as we
    want to share the layout styles.
    
    I've also updated it to use SSR to handle whether to display
    the login or start buttons. This helps prevent a flash of content
    that is not available to unauthenticated users

--- components/Game.js
+++ components/Game.js
@@ -3,21 +3,6 @@ import GameButton from './GameButton'
 
 // TODO: Start/join games should probably be a route or at least url params so we can go back with browser controls
 const Game = () => {
-  return (
-    <VStack align="stretch" spacing={16} py={40}>
-      <Heading as="h1" align="center" size="3xl">
-        Scorecard
-      </Heading>
-
-      <VStack align="stretch" spacing={6} px={20}>
-        <GameButton link="/lobby" type="start">
-          Start a game
-        </GameButton>
-        <GameButton link="/lobby" type="join">
-          Join a game
-        </GameButton>
-      </VStack>
-    </VStack>
-  )
+  return <>Game!</>
 }
 export default Game
