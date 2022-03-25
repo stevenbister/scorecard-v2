1571cc9ab90e844eaf02ca9ba3db1d39991143d6
Steven Bister <stevebister@gmail.com>
2022-03-25 12:19:06 +0000

    Test: Installed dotenv so we can pass our env variables into Cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,6 +11,7 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+require('dotenv').config({ path: '.env.test.local' }) // Probs need to make this a little more flexible
 
 /**
  * @type {Cypress.PluginConfig}
@@ -19,4 +20,9 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  config.env.username = process.env.SUPABASE_TEST_USER_EMAIL
+  config.env.password = process.env.SUPABASE_TEST_USER_PASSWORD
+
+  return config
 }
