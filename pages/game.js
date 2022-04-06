c7c07a4b8b9a1002472cefbfdd94712c791d192b
Steven Bister <stevebister@gmail.com>
2022-04-06 16:48:35 +0100

    Feat: Adds a useScore hook to set and push the players score into the db

--- pages/game.js
+++ pages/game.js
@@ -1,21 +1,22 @@
-import { useState, useContext } from 'react'
+import { useContext } from 'react'
 import { GameContext } from '../lib/game/GameContext'
 import { SimpleGrid, Box, VStack, Flex } from '@chakra-ui/react'
 import GameAlertButton from '../components/GameAlertButton'
-import Leaderboard from '../components/LeaderBoard'
+import Leaderboard from '../components/Leaderboard'
 import ScoreInput from '../components/ScoreInput'
 import { arrayValuesToNumbers, sumArray } from '../utils/arrays'
+import { useScore } from '../lib/game/useScore'
 
 const Game = () => {
-  const [score, setScore] = useState(0)
-  const { pin, players, host } = useContext(GameContext)
+  const [score, setScore] = useScore(0)
+  const { pin, host, rounds, setRounds } = useContext(GameContext)
 
   const handleScoreChange = (e) => {
     const { value } = e.currentTarget
+    setRounds(value) // Save the current input to the local storage so we can refresh the page if necessary
+
     const valueToArray = arrayValuesToNumbers(value.split('\n'))
     const totalScore = sumArray(valueToArray)
-
-    console.log(valueToArray)
     setScore(totalScore)
   }
 
@@ -29,11 +30,9 @@ const Game = () => {
       <Box>Pin:{pin}</Box>
 
       <SimpleGrid columns={2} spacing={4}>
-        <div>
-          <ScoreInput onChange={handleScoreChange} />
-        </div>
+        <ScoreInput defaultValue={rounds} onChange={handleScoreChange} />
 
-        <Leaderboard players={players} score={score} />
+        <Leaderboard />
       </SimpleGrid>
 
       <Flex
